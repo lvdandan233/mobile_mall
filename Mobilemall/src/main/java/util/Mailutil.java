b9170c4ca36d57a2507465ad3893b935e20e876e
wangyulong <wangyulong@xiyun.com.cn>
2022-07-05 09:53:37 +0800

    test

--- Mobilemall/src/main/java/util/Mailutil.java
+++ Mobilemall/src/main/java/util/Mailutil.java
@@ -19,7 +19,7 @@ public class Mailutil {
 //五个参数,第一个是发件人信息,第二个是收件人,第三个是邮件的主题,第四个是邮件的内容.
 // 第五个是邮件内容是否以html格式展示
             cn.hutool.extra.mail.MailUtil.send(account,acceptor ,
-                    "天狼星手机商城激活","欢迎注册天狼星商城的会员，点击链接完成注册激活，祝您在天狼星手机商城购物愉快！激活链接：<a href='http://localhost:8081/#/reg_ok''>点击激活</a>", true);
+                    "天狼星手机商城激活","欢迎注册天狼星商城的会员，点击链接完成注册激活，祝您在天狼星手机商城购物愉快！激活链接：<a href='http://localhost:8080/#/reg_ok''>点击激活</a>", true);
         } catch (Exception e) {
             e.printStackTrace();
         }
